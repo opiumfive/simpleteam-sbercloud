42e55f23f80b747894b96942b328238eca192c8c
Alexey Petrushin <petrushinalex@gmail.com>
2021-02-27 14:34:34 +0300

    icon for dashboard

--- lib/ui/common/icon_widget.dart
+++ lib/ui/common/icon_widget.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SberIcon extends StatelessWidget {
@@ -11,11 +9,21 @@ class SberIcon extends StatelessWidget {
   static const Configuration = "ic_configuration.svg";
   static const Dashboard = "ic_dashboard.svg";
   static const Settings = "ic_settings.svg";
+  static const Alarm = "ic_alarm.svg";
+  static const Edit = "ic_edit.svg";
+  static const Profile = "ic_profile.svg";
+  static const ArrowUp = "ic_arrow_up.svg";
+  static const ArrowDown = "ic_arrow_down.svg";
+
+  static const Eye12dp = "ic_eye_12dp.svg";
+  static const Close12dp = "ic_close_12dp.svg";
+  static const Close16dp = "ic_close_16dp.svg";
+  static const Plus40dp = "ic_plus_40dp.svg";
 
   const SberIcon(
       this.icon, {
         Key key,
-        this.size = 24.0,
+        this.size,
         this.color,
       }) : super(key: key);
 
